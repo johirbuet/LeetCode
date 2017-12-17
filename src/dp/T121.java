fec39032c29180c592d08a34130dfaf990ad2241
johirbuet <johirbuet@gmail.com>
2017-12-16 22:04:56 -0600

    Test case added

--- src/dp/T121.java
+++ src/dp/T121.java
@@ -17,6 +17,8 @@ public class T121 {
 	public void test() {
 		int [] P = {7, 1, 5, 3, 6, 4};
 		assertEquals(5, p.maxProfit(P));
+		int [] Q = {7,5,4,3,2,1};
+		assertEquals(0, p.maxProfit(Q));
 	}
 
 }
